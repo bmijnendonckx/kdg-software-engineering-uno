f39220ff912cd89fb7c9b8006aca8d89af313784
Billy Mijnendonckx <billy.mijnendonckx@student.kdg.be>
2016-10-18 13:26:28 +0200

    standard Card class added

---
+++ Assets/Card.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public bool IsDraggable {
+        get; set;
+    }
+
+    public bool IsBeingDragged {
+        get; set;
+    }
+
+    public Image GetImage {
+        get; set;
+    }
+
+    public bool CanBePlayed () {
+        return true;
+    }
+
+    public void ReturnCard() {
+
+    }
+
+    public void CreateGameobject() {
+
+    }
+
+    public void OnPlay() {
+
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+    }
+}
