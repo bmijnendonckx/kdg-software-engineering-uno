2f5a21586b0bd367b46c1d0b299a64e5faa39453
Billy Mijnendonckx <billy.mijnendonckx@student.kdg.be>
2016-10-28 16:49:34 +0200

    Big Commit: pseudo-playable, arranged MVC, Added Tags, Added 2nd Pile, Draggable Cards

---
+++ Assets/Scripts/Card/CardController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public abstract class CardController : ScriptableObject {
+    public CardModel model;
+
+    public abstract bool CanBePlayed();
+
+    public CardView CreateGameobject() {
+        CardView i = Instantiate(Resources.Load<CardView>("Card"));
+
+        i.gameObject.GetComponent<Image>().sprite = model.cardFace;
+
+        i.transform.SetParent(GameObject.FindGameObjectWithTag("Pile").transform);
+        i.controller = this;
+        return i;
+    }
+
+    public virtual void OnPlay() {
+
+    }
+}
---
+++ Assets/Scripts/Card/CardView.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public Sprite cardBack;
+    public CardController controller;
+
+    public void Awake() {
+        IsDraggable = false;
+
+        if(gameObject.tag != "CurrentCard") {
+            IsDraggable = true;
+        }
+    }
+
+    public bool IsDraggable {
+        get; set;
+    }
+
+    public bool IsBeingDragged {
+        get; set;
+    }
+
+    public Sprite CardFace {
+        get {
+            return GetComponent<Image>().sprite;
+        }
+        set {
+            Image image = GetComponent<Image>();
+            image.sprite = value;
+        }
+    }
+
+    public bool CanBePlayed () {
+        return controller.CanBePlayed();
+    }
+
+    public void ReturnCard() {
+        transform.SetParent(previousParent);
+    }
+
+    public virtual void OnPlay() {
+        controller.OnPlay();
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        if(IsDraggable) {
+            this.transform.position = eventData.position;
+        }
+    }
+
+    Transform previousParent;
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        if(IsDraggable) {
+            IsBeingDragged = true;
+            previousParent = transform.parent;
+            transform.SetParent(transform.parent.parent);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        if(IsDraggable) {
+            RaycastResult rr = eventData.pointerCurrentRaycast;
+            if(rr.gameObject.tag == "CurrentCard") {
+                CurrentCard.setCurrentCard(gameObject);
+                rr.gameObject.GetComponent<CurrentCard>().onChangeCard();
+                Debug.Log(CurrentCard.currentCard());
+            } else
+            ReturnCard();
+        }
+    }
+}
