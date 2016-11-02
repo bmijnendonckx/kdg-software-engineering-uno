a5413d366027a8dd957b3f83c6de64efd7eaaff5
Thomas Vanvelk <thomas.vanvelk@telenet.be>
2016-11-02 09:29:24 +0100

    ColorSelect Functional Bugs: Background Color Doesn't Change, Currently 4 functions to change color, because eventlisteners don't take parameters.

--- Assets/Scripts/ColorSelectCard.cs
+++ Assets/Scripts/ColorSelectCard.cs
@@ -17,7 +17,40 @@ public class ColorSelectCard :CardController {
         ColorChangeMenu.transform.SetAsLastSibling();
         ColorChangeMenu.transform.localPosition = new Vector3(0, 300, 0);
 
-        //ColorChangeMenu.transform.Find("red").GetComponent<Button>().onClick.AddListener();
+        ColorChangeMenu.transform.Find("red").GetComponent<Button>().onClick.AddListener(ChangeToRed); //Can't give a parameter in an addlisterer. Need to find a better solution
+        ColorChangeMenu.transform.Find("blue").GetComponent<Button>().onClick.AddListener(ChangeToBlue);
+        ColorChangeMenu.transform.Find("green").GetComponent<Button>().onClick.AddListener(ChangeToGreen);
+        ColorChangeMenu.transform.Find("Yellow").GetComponent<Button>().onClick.AddListener(ChangeToYellow);
+    }
+
+    public static void ChangeToRed()
+    {
+        CurrentCard.Color = "red";
+        Debug.Log(CurrentCard.Color);
+        GameManager.createContinueUI();
+        Destroy(GameObject.FindGameObjectWithTag("ColorSelect"));
+ 
+    }
+    public static void ChangeToBlue()
+    {
+        CurrentCard.Color = "blue";
+        Debug.Log(CurrentCard.Color);
+        GameManager.createContinueUI();
+        Destroy(GameObject.FindGameObjectWithTag("ColorSelect"));
+    }
+    public static void ChangeToGreen()
+    {
+        CurrentCard.Color = "green";
+        Debug.Log(CurrentCard.Color);
+        GameManager.createContinueUI();
+        Destroy(GameObject.FindGameObjectWithTag("ColorSelect"));
+    }
+    public static void ChangeToYellow()
+    {
+        CurrentCard.Color = "yellow";
+        Debug.Log(CurrentCard.Color);
+        GameManager.createContinueUI();
+        Destroy(GameObject.FindGameObjectWithTag("ColorSelect"));
     }
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour {
     static Player[] players = new Player[2];
     static int playerIndex = 0;
     static GameObject continueUI;
+    public Color red, yellow, blue, green;
     static GameManager instance;
 
     public static int PlayerIndex {
@@ -85,5 +86,9 @@ public class GameManager : MonoBehaviour {
     public static void ToggleNextPlayer() {
         PlayerIndex++;
         BeginTurn();
+    }
+    public static void ChangeColor(string NewColor)
+    {
+        
     }
 }
