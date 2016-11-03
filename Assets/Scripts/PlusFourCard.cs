43d286f5921344c2b80a93182fa25687511e188b
Thomas Vanvelk <thomas.vanvelk@telenet.be>
2016-11-03 16:48:44 +0100

    added playability rule to +4 color change

--- Assets/Scripts/PlusFourCard.cs
+++ Assets/Scripts/PlusFourCard.cs
@@ -4,7 +4,17 @@ using System.Collections;
 [CreateAssetMenu(fileName = "Cards/PlusFourCard", menuName = "PlusFourCard")]
 public class PlusFourCard : ColorSelectCard {
     public override bool CanBePlayed() {
-        return true;
+        bool IsPlayable = true;
+        for (int i = 0; i < GameManager.CurrentPlayer.hand.Count; i++)
+        {
+            
+            if (GameManager.CurrentPlayer.hand[i].controller.Model.color == CurrentCard.Color)
+            {
+                IsPlayable = false;
+            }
+           
+        }
+        return IsPlayable;
     }
 
     public override void OnPlay() {
